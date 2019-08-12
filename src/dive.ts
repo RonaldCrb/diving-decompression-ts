05d70836b469e13bb7a6e87de9c2fdfcec0f97d7
Ron Alonzo <alonzo.ronald@gmail.com>
2019-08-12 13:07:57 +0200

    initial commit

---
+++ src/dive.ts
@@ -0,0 +1,7 @@
+export function noDecompressionLimit({ depth, bottomTime }) {
+  console.log(`the no decompression limit for ${bottomTime} minutes at ${depth} is ABC minutes`);
+}
+
+export function groupLetter({ depth, bottomTime }) {
+  console.log(`the group letter for ${bottomTime} minutes at ${depth} is X`);
+}
